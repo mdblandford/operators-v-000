7e5bb2cc030074c9e466cbebe630f6a087e57751
Michael Blandford <mdblandford@gmail.com>
2018-06-18 18:37:44 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,9 @@
 def unsafe?(speed)
-  if speed > 60 || speed < 40 
-    true 
-  else false
-  end
+  speed > 60 || speed < 40 ? true : false
 end
 
 
 
 def not_safe?(speed)
-  speed > 60 || speed < 40 ? true : false	
+  speed > 60 || speed < 40 ? true : false
 end
-	
-
-
